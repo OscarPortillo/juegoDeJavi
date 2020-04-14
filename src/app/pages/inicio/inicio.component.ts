99e1f33d295f2fb7cd7c38ae562707de069ff668
javiperales <javiperales95@gmail.com>
2020-04-14 19:31:31 +0200

    **

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -7,7 +7,16 @@ import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
   private arrayColores=["red","yellow","blue","purple","pink","green","brown","violet"];
-  private pruebas=["El/La jugador de mi derecha bebe 2 tragos", "Yo nunca nunca he perreado con BadBunny"];
+  private pruebas=["El/La jugador de mi derecha bebe 2 tragos", 
+  "Yo nunca nunca he perreado con BadBunny",
+"El/La mas mayor del grupo bebe 3 tragos",
+"El/La mas menor del grupo bebe 3 tragos",
+"El/La mas mayor del grupo mandar beber 6 tragos(repartidos)",
+"El/La mas menor del grupo mandar beber 6 tragos(repartidos)",
+
+
+
+];
 
   public titulo : string;
     constructor(private elementRef: ElementRef){
